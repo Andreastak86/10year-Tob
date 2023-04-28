bb63ce52d81242bf0bd6ef2936022367791be533
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2023-04-28 13:49:59 +0200

    smal visuals

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,19 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
     return (
         <main className=' flex flex-col items-center mx-auto gap-4 mt-4 px-4'>
-            <h1 className=' text-[25px]'>Gratulerer med 10-års dagen Tobias</h1>
+            <h1 className=' text-[25px] text-center'>
+                Gratulerer med 10-års dagen Tobias
+            </h1>
             <h3>Nå er det bare:</h3>
             <div className=' text-white font-extrabold text-[40px] text-center'>
                 <Countdown />
             </div>
-            <h3>Til du får Zelda</h3>
-            <Image src={Zelda} alt='Bilde av nye Zelda' width={500} />
+            <h3 className='text-[24px]'>Til du kan spille Zelda</h3>
+            <Image src={Zelda} alt='Bilde av nye Zelda' width={320} />
             <h3>Den første traileren</h3>
             <iframe
-                width='500'
-                height='315'
+                width='320'
+                height='210'
                 src='https://www.youtube.com/embed/2SNF4M_v7wc'
                 title='YouTube video player'
                 frameborder='0'
@@ -28,8 +30,8 @@ export default function Home() {
 
             <h3>Trailer nr. 2</h3>
             <iframe
-                width='500'
-                height='315'
+                width='320'
+                height='210'
                 src='https://www.youtube.com/embed/fYZuiFDQwQw'
                 title='YouTube video player'
                 frameborder='0'
@@ -39,8 +41,8 @@ export default function Home() {
 
             <h3>Trailer nr. 3</h3>
             <iframe
-                width='500'
-                height='315'
+                width='320'
+                height='210'
                 src='https://www.youtube.com/embed/uHGShqcAHlQ'
                 title='YouTube video player'
                 frameborder='0'

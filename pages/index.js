09d87a6eee3d97cd59483e539e49047faf7db3c9
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2023-04-28 14:25:05 +0200

    ...

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <main className=' flex flex-col items-center mx-auto gap-4 mt-4 px-4'>
+        <main className=' flex flex-col items-center mx-auto gap-4 my-4 px-4'>
             <h1 className=' text-[25px] text-center'>
                 Gratulerer med 10-års dagen Tobias
             </h1>
@@ -16,6 +16,7 @@ export default function Home() {
                 <Countdown />
             </div>
             <h3 className='text-[24px]'>Til du kan spille Zelda</h3>
+            <p>Spillet får du i bursdagsgave av mamma og pappa</p>
             <Image src={Zelda} alt='Bilde av nye Zelda' width={320} />
             <h3>Den første traileren</h3>
             <iframe
